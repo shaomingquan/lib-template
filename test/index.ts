63476eb4d7bebafb5c4ce065a775b3271714de88
shaomingquan <shaomingquan@meituan.com>
2019-05-08 23:00:28 +0800

    code fommatters

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,13 @@
-import {describe, it} from 'mocha'
-const assert = require('assert')
+import { describe, it } from 'mocha';
 
-import { add } from '../src/index'
+import assert from 'assert';
 
-describe('index', function () {
-    describe('add', function () {
+import { add } from '../src/index';
+
+describe('index', function() {
+    describe('add', function() {
         it('should work well', function() {
-            assert.equal(add(1, 2), 3)
-        })
-    })
-})
+            assert.equal(add(1, 2), 3);
+        });
+    });
+});
